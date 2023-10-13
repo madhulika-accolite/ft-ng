215c237e1fc9c5a044d49a7e2988606c8892a364
Madhulika Mukherjee <madhulika.m@accolite.com>
2023-10-13 18:44:46 +0530

    timeseries chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
-
+import * as FusionCharts from 'fusioncharts';
+const dataUrl =
+  'https://raw.githubusercontent.com/fusioncharts/dev_centre_docs/master/assets/datasources/fusiontime/examples/online-sales-single-series/data.json';
+const schemaUrl =
+  'https://raw.githubusercontent.com/fusioncharts/dev_centre_docs/master/assets/datasources/fusiontime/examples/online-sales-single-series/schema.json';
+let dataSource = {
+  data: null,
+  chart: {
+    // exportEnabled: 1
+  },
+  caption: {
+    text: 'Online Sales of a SuperStore in the US'
+  },
+  yAxis: {
+    "plot": {
+      "value": "Sales",
+      "type": "line"
+    },
+  }
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +26,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ft-ng';
+  dataSource: any;
+  events: any;
+  type: string;
+  width: string;
+  height: string;
+  constructor() {
+    this.type = 'timeseries';
+    this.width = '100%';
+    this.height = '400';
+    this.dataSource = dataSource
+    this.events = {
+      customRangeSelect: function (eventObj:any) {
+        console.log(eventObj.data.start); //start timestamp
+        console.log(eventObj.data.end); //end timestamp
+        setTimeout(() => {
+          dataSource.caption.text ="Chart Title Updated";
+          dataSource.yAxis.plot.type ="column";
+          eventObj.sender.setJSONData(dataSource)
+        }, 2000);
+       
+      }
+    }
+    this.fetchData();
+  }
+  fetchData() {
+    let jsonify = (res: any) => res.json();
+    let dataFetch = fetch(dataUrl).then(jsonify);
+    let schemaFetch = fetch(schemaUrl).then(jsonify);
+    Promise.all([dataFetch, schemaFetch]).then(res => {
+      let data = res[0];
+      let schema = res[1];
+      let fusionTable = new FusionCharts.DataStore().createDataTable(
+        data,
+        schema
+      ); // Instance of DataTable to be passed as data in dataSource
+      this.dataSource.data = fusionTable;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// Import angular-fusioncharts
+import { FusionChartsModule } from 'angular-fusioncharts';
+// Import FusionCharts library and chart modules
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+import * as TimeSeries from 'fusioncharts/fusioncharts.timeseries'; // Import timeseries
+// Pass the fusioncharts library and chart modules
+FusionChartsModule.fcRoot(FusionCharts, Charts, TimeSeries);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
